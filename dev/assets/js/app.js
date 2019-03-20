3a046d38adc4732d136aebcb0ab3e095340e770b
middls <middls647@gmail.com>
2019-03-20 17:08:36 +0300

    commit

---
+++ dev/assets/js/app.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    $('.spoiler-body').hide();
+    $('.spoiler-title').click(function(){
+        $(this).toggleClass('opened').toggleClass('closed').next().slideToggle();
+        if($(this).hasClass('opened')) {
+        }
+    });
+});
--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -1,2 +1,2 @@
-"use strict";
+"use strict";$(document).ready(function(){$(".spoiler-body").hide(),$(".spoiler-title").click(function(){$(this).toggleClass("opened").toggleClass("closed").next().slideToggle(),$(this).hasClass("opened")})});
 //# sourceMappingURL=app.js.map
