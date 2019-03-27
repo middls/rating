8c468780f8e23080f41aa3bebf18b441a54221a3
middls <middls647@gmail.com>
2019-03-27 12:29:48 +0300

    commit

--- dev/assets/js/app.js
+++ dev/assets/js/app.js
@@ -8,3 +8,50 @@ $(document).ready(function(){
     });
 });
 
+$(function() {
+
+	$('.btn-catalog').on('click', function() {
+		$()
+		return false
+	})
+
+	$('.btn-catalog').on('click', function() {
+		$('.inside-catalog').slideToggle('fast')
+		return false
+	})
+
+
+	$('.btn-menu').on('click', function() {
+		$('.menu-catalog').addClass('active')
+		$('html').css({ overflowY: 'hidden' })
+		return false
+	})
+
+	$('.btn-filter').on('click', function() {
+		$('.filtermenu').addClass('active')
+		$('html').css({ overflowY: 'hidden' })
+		return false
+	})
+
+	$('.menu-catalog-head').on('click', function() {
+		$('.menu-catalog').removeClass('active')
+		$('html').css({ overflowY: 'inherit' })
+		return false
+	})
+	
+    $('.filtermenu-head').on('click', function() {
+    	$('.filtermenu').removeClass('active')
+		$('html').css({ overflowY: 'inherit' })
+    	return false
+    })
+
+	$('.modal').on('show.bs.modal', function (e) {
+		$(this).find('select').selectmenu()
+	})
+
+	$('.btn-up').click(function() {
+	    $('html, body').animate({scrollTop: 0},500);
+	    return false;
+	})
+
+})
